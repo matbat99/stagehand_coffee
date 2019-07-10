443f4c8519354f6942a9ab60772922e4e8d763a0
Matthew Bates <matbat99@yahoo.com>
2019-07-10 20:10:45 +0200

    adds coffee shop model

---
+++ db/migrate/20190710180950_create_coffees.rb
@@ -0,0 +1,10 @@
+class CreateCoffees < ActiveRecord::Migration[5.2]
+  def change
+    create_table :coffees do |t|
+      t.string :name
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
